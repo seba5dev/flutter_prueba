44ed0179c9076ae3d5b02b27441c08826ccfa8d4
Sebastian Martínez <35276965+smartinez05@users.noreply.github.com>
2021-11-08 21:42:07 -0500

    cambios en busquedas y anadir base resultados

---
+++ lib/busqueda.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class busqueda extends StatefulWidget {
+  @override
+  busquedaApp createState() => busquedaApp();
+}
+
+class busquedaApp extends State<busqueda> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registro de busqueda'),
+      ),
+    );
+  }
+}
